aa0c47c28c520cb48a0c6f93ce7b7f79990a7631
OmkarJ <oj9493@gmail.com>
2014-02-08 05:11:32 -0500

    Update file02.sh

--- file02.sh
+++ file02.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 #loopback is edited
 sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/my.cnf
 
@@ -20,28 +21,27 @@ PASSWORD('$MYSQL_PASS');"
 mysql -uroot -p$MYSQL_PASS -e "SET PASSWORD FOR 'nova'@'localhost' =
 PASSWORD('$MYSQL_PASS');"
 
-sudo "--sql_connection=mysql://nova:project@172.16.0.1/nova">>/etc/nova/nova.conf
+echo "--sql_connection=mysql://nova:project@172.16.0.1/nova">>/etc/nova/nova.conf
 
+echo "--use_deprecated_auth">>/etc/nova/nova.conf
 
-sudo "--use_deprecated_auth">>/etc/nova/nova.conf
+echo "--s3_host=172.16.0.1">>/etc/nova/nova.conf
 
-sudo "--s3_host=172.16.0.1">>/etc/nova/nova.conf
+echo "--rabbit_host=172.16.0.1">>/etc/nova/nova.conf
 
-sudo "--rabbit_host=172.16.0.1">>/etc/nova/nova.conf
+echo "--ec2_host=172.16.0.1">>/etc/nova/nova.conf
 
-sudo "--ec2_host=172.16.0.1">>/etc/nova/nova.conf
+echo "--ec2_dmz_host=172.16.0.1">>/etc/nova/nova.conf
 
-sudo "--ec2_dmz_host=172.16.0.1">>/etc/nova/nova.conf
+echo "--public_interface=eth1">>/etc/nova/nova.conf
 
-sudo "--public_interface=eth1">>/etc/nova/nova.conf
+echo "--image_service=nova.image.glance.GlanceImageService">>/etc/nova/nova.conf
 
-sudo "--image_service=nova.image.glance.GlanceImageService">>/etc/nova/nova.conf
+echo "--glance_api_servers=172.16.0.1:9292">>/etc/nova/nova.conf
 
-sudo "--glance_api_servers=172.16.0.1:9292">>/etc/nova/nova.conf
+echo "--auto_assign_floating_ip=true">>/etc/nova/nova.conf
 
-sudo "--auto_assign_floating_ip=true">>/etc/nova/nova.conf
-
-sudo "--scheduler_default_filters=AllHostsFilter">>/etc/nova/nova.conf
+echo "--scheduler_default_filters=AllHostsFilter">>/etc/nova/nova.conf
 
 sudo sed -i 's/kvm/qemu/g' /etc/nova/nova-compute.conf
 
